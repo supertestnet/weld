25a52a1f7dad224762869bbf62e7d6c4acafc7ba
supertestnet <58400631+supertestnet@users.noreply.github.com>
2024-10-12 00:53:49 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+import init, { Offer, Invoice } from "./boltz_bolt12.js"
+
+const run = async () => {
+  const bolt12 = await init("./boltz_bolt12_bg.wasm");
+
+  const offer = new Offer(
+      "lno1zrxq8pjw7qjlm68mtp7e3yvxee4y5xrgjhhyf2fxhlphpckrvevh50u0qtuzuc7vrxw3ptvmvzs7lqmyp94rhmfdg6gpef65rs2xxl2mzr2ksqszz0xxdd9y6653lwugyjqg6nwjerz4wcdskllmkp9fl4kp4d5z0hfqqvlxj7wm32qk0pavw6cr3kfn6unhzy9r25vlp4nmmy0gfck4ur7qk0xz43lhmnlx59qfxmdh6ue6y65s7rw4qdtdltt5gzratf3hnr668laqtcrk5tyzfz8a8ckq4dlpd40alv646qqsf6pgykhxlayyswqmjffpgehjjs"
+    );
+    console.log(offer.id)
+  offer.free();
+};
+
+run();
